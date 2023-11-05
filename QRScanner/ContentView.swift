d6d416cb764d39ccd6d43926a581fd7b9b4c2ece
MAHESHWARAN <maheshwaran@eam360.com>
2023-11-05 07:46:50 +0530

    ContentView  - Updated scannerView

--- QRScanner/ContentView.swift
+++ QRScanner/ContentView.swift
@@ -11,71 +11,108 @@ import AVKit
 struct ContentView: View {
   
   @Environment(\.dismiss) private var dismiss
-  @State private var isScanning = false
-  @State private var session = AVCaptureSession()
-  @State private var qrOutput = AVCaptureMetadataOutput()
-  
-  @State private var errorMessage: String?
-  @StateObject private var qrOutputDelegate = QRScannerDelegate()
-  @State private var outpuQRCode: String?
+  @StateObject private var viewModel = ScannerViewModel()
+  @State private var selectedCode: String?
   
   var body: some View {
     mainView
+      .overlay(alignment: .topLeading, content: headerView)
+      .safeAreaInset(edge: .bottom, content: scanButton)
+      .onAppear(perform: viewModel.checkAccessForCamera)
   }
   
   private var mainView: some View {
+    GeometryReader { proxy in
+      roundRectangleView(proxy)
+        .background {
+          scannerView(proxy)
+        }
+    }
+    .alert("Camera Disabled", isPresented: $viewModel.showCameraAlert) {
+      Button("Cancel") {}
+      if let url = URL(string: UIApplication.openSettingsURLString) {
+        Link("Settings", destination: url)
+      }
+    } message: {
+      Text("Please Enable Camera access for QRScanner")
+    }
+    .onReceive(NotificationCenter.default.publisher(
+      for: UIApplication.didEnterBackgroundNotification)) { _ in
+        viewModel.stopRunning()
+      }
+      .accessibilityElement(children: .contain)
+  }
+  
+  private func roundRectangleView(_ proxy: GeometryProxy) -> some View {
+    ZStack {
+      ForEach(0...3, id: \.self) { index in
+        RoundedRectangle(cornerRadius: 16, style: .circular)
+          .trim(from: 0.58, to: 0.66)
+          .stroke(Color.primary,
+                  style: .init(lineWidth: 2, lineCap: .round, lineJoin: .round))
+          .rotationEffect(.init(degrees: Double(index) * 90))
+      }
+    }
+    .frame(width: 320, height: 320)
+    .preference(key: SizeKey.self, value: proxy.size)
+    .onPreferenceChange(SizeKey.self) {
+      viewModel.updateOutputRectOfInterest($0)
+    }
+    .ignoresSafeArea(.all)
+    .frame(maxWidth: .infinity, maxHeight: .infinity)
+  }
+  
+  
+  private func scannerView(_ proxy: GeometryProxy) -> some View {
+    ZStack {
+      if let cameraLayer = viewModel.previewLayer {
+        ScannerInputView(
+          frameSize: .init(width: proxy.size.width, height: proxy.size.height),
+          cameraLayer: cameraLayer)
+        .background(Color.gray)
+      }
+    }
+  }
+  
+}
+
+extension ContentView {
+  func headerView() -> some View {
     VStack(spacing: 8) {
-      closeButton
+      titleView
       placeholderTextView
       qrCodeTextView
       
-      scannerView
-      scanButton
     }
+    .frame(maxWidth: .infinity)
+    .overlay(alignment: .topLeading, content: cancelButton)
     .padding(.horizontal, 10)
-    .frame(maxWidth: .infinity, alignment: .topLeading)
-    .alert("Settings", isPresented: errorBinding) {
-      Button("Cancel", action: {})
-      
-      if let url = URL(string: UIApplication.openSettingsURLString) {
-        Link("Open", destination: url)
-      }
-    } message: {
-      Text(errorMessage ?? "")
-    }
-    .onAppear {
-      checkForCameraPermissions()
-    }
-    .onChange(of: qrOutputDelegate.scannedCode) {
-      guard let value = $0 else { return }
-      session.stopRunning()
-      resetAnimationView()
-      qrOutputDelegate.scannedCode = .none
-      outpuQRCode = value
-    }
+    .frame(maxWidth: .infinity, alignment: .leading)
+    .accessibilityElement(children: .contain)
   }
   
-  private var closeButton: some View {
-    Button {
-      dismiss()
-    } label: {
-      Image(systemName: "xmark")
-        .renderingMode(.template)
-        .font(.title3)
-        .foregroundStyle(Color.secondary)
-    }
-    .frame(maxWidth: .infinity, alignment: .leading)
+  private var titleView: some View {
+    Text("Scanner")
+      .font(.title3)
+      .foregroundStyle(Color.primary)
+      .padding(.horizontal, 16)
+      .padding(.vertical, 10)
+      .background(Color.gray.opacity(0.5))
+      .clipShape(Capsule())
   }
   
   private var placeholderTextView: some View {
-    Text("Place the QR code inside the frame")
+    Text("Keep the QR code inside the frame")
       .font(.headline)
       .foregroundStyle(Color.primary)
+      .padding(10)
+      .background(Color.gray.opacity(0.5))
+      .clipShape(Capsule())
   }
   
   @ViewBuilder
   private var qrCodeTextView: some View {
-    if let scannedCode = outpuQRCode {
+    if let scannedCode = viewModel.scannedCode {
       Text(scannedCode)
         .font(.headline)
         .foregroundStyle(Color.primary)
@@ -86,133 +123,32 @@ struct ContentView: View {
     }
   }
   
-  // MARK: - Scanner View
-  
-  private var scannerView: some View {
-    GeometryReader { proxy in
-      
-      ZStack {
-        CameraView(frameSize: .init(width: proxy.size.width/2,
-                                    height: proxy.size.width/2), session: $session)
-        .clipShape(RoundedRectangle(cornerRadius: 8))
-        
-        
-        ForEach(0...3, id: \.self) { index in
-          RoundedRectangle(cornerRadius: 8, style: .circular)
-            .trim(from: 0.58, to: 0.66)
-            .stroke(Color.primary,
-                    style: .init(lineWidth: 1.5, lineCap: .round, lineJoin: .round))
-            .rotationEffect(.init(degrees: Double(index) * 90))
-          
-        }
-      }
-      .frame(width: proxy.size.width/2, height: proxy.size.width/2)
-      .overlay(alignment: .top) { overlayScannerView(proxy) }
-      .frame(maxWidth: .infinity, maxHeight: .infinity)
-    }
-  }
-  
-  private func overlayScannerView(_ proxy: GeometryProxy) -> some View {
-    RoundedRectangle(cornerRadius: 25.0)
-      .fill(.primary)
-      .frame(height: 1)
-      .shadow(color: .black.opacity(0.5), radius: 8, x: 0, y: isScanning ? 15 : -15)
-      .offset(y: isScanning ? proxy.size.width/2 : 0)
-      .padding(.horizontal, 5)
-  }
-  
-  private func scanAnimationView() {
-    withAnimation(.easeInOut(duration: 0.85).delay(0.2).repeatForever(autoreverses: true)) {
-      isScanning = true
-    }
-  }
-  
-  private func resetAnimationView() {
-    withAnimation(.easeInOut(duration: 0.85)) {
-      isScanning = false
-    }
-  }
-  
-  private var scanButton: some View {
+  private func cancelButton() -> some View {
     Button {
-      if !session.isRunning {
-        activateCameraView()
-        scanAnimationView()
-      }
+      dismiss()
+    } label: {
+      Image(systemName: "chevron.left")
+    }
+  }
+  
+  private func scanButton() -> some View {
+    Button {
+      viewModel.startSession()
     } label: {
       Image(systemName: "qrcode.viewfinder")
         .font(.largeTitle)
         .foregroundStyle(Color.primary)
     }
   }
+}
+
+extension ContentView {
   
-  // MARK: - Camera
-  
-  private func setupCamera() {
-    do {
-      guard let device = AVCaptureDevice
-        .DiscoverySession(
-          deviceTypes: [.builtInWideAngleCamera],
-          mediaType: .video, position: .back).devices.first else {
-        return
-      }
-      let input = try AVCaptureDeviceInput(device: device)
-      
-      guard session.canAddInput(input), session.canAddOutput(qrOutput) else {
-        return
-      }
-      session.beginConfiguration()
-      session.addInput(input)
-      session.addOutput(qrOutput)
-      
-      qrOutput.metadataObjectTypes = [.qr]
-      qrOutput.setMetadataObjectsDelegate(qrOutputDelegate, queue: .main)
-      session.commitConfiguration()
-      
-      activateCameraView()
-      scanAnimationView()
-    } catch {
-      errorMessage = error.localizedDescription
-    }
-  }
-  
-  private func activateCameraView() {
-    DispatchQueue.global(qos: .background).async {
-      session.startRunning()
-    }
-  }
-  
-  // MARK: - Error
-  
-  private var errorBinding: Binding<Bool> {
-    Binding {
-      errorMessage != nil
-    } set: {
-      if !$0 {
-        errorMessage = nil
-      }
-    }
-  }
-  
-  private func checkForCameraPermissions() {
-    Task {
-      switch AVCaptureDevice.authorizationStatus(for: .video) {
-      case .authorized:
-        if session.inputs.isEmpty {
-          setupCamera()
-        } else {
-          session.startRunning()
-        }
-      case .notDetermined:
-        if await AVCaptureDevice.requestAccess(for: .video) {
-          setupCamera()
-        } else {
-          errorMessage = "Please provide camera access to scan QR codes"
-        }
-      case .denied, .restricted:
-        errorMessage = "Please provide camera access to scan QR codes"
-      default: break
-      }
+  struct SizeKey: PreferenceKey {
+    static var defaultValue = CGSize.zero
+    
+    static func reduce(value: inout CGSize, nextValue: () -> CGSize) {
+      value = nextValue()
     }
   }
 }
---
+++ QRScanner/ScannerViewModel.swift
@@ -0,0 +1,110 @@
+//
+//  ScannerViewModel.swift
+//  QRScanner
+//
+//  Created by MAHESHWARAN on 28/10/23.
+//
+
+import SwiftUI
+import AVKit
+
+class ScannerViewModel: NSObject, ObservableObject {
+  
+  var session: AVCaptureSession?
+  var output = AVCaptureMetadataOutput()
+  var previewLayer: AVCaptureVideoPreviewLayer?
+  
+  @Published var scannedCode: String?
+  @Published var showCameraAlert = false
+  
+  override init() {
+    super.init()
+    setupSession()
+  }
+  
+  private func setupSession() {
+    guard session == nil,
+          let device = AVCaptureDevice.default(
+            .builtInWideAngleCamera, for: .video, position: .back) else {
+      return
+    }
+    do {
+      let input = try AVCaptureDeviceInput(device: device)
+      let session = AVCaptureSession()
+      
+      guard session.canAddInput(input), session.canAddOutput(output) else {
+        return
+      }
+      session.beginConfiguration()
+      session.addInput(input)
+      session.addOutput(output)
+      
+      output.metadataObjectTypes = [
+        .qr, .ean8, .ean13, .pdf417, .upce,
+        .code93, .code128, .code39, .aztec,
+        .itf14, .dataMatrix
+      ]
+      output.setMetadataObjectsDelegate(self, queue: .main)
+      output.connection(with: .video)?.preferredVideoStabilizationMode = .standard
+      session.commitConfiguration()
+      previewLayer = .init(session: session)
+      
+      self.session = session
+      startSession()
+    } catch {
+      debugPrint("Error while capturing video input, error: \(error.localizedDescription)")
+    }
+  }
+  
+  func startSession() {
+    guard !(session?.isRunning ?? false) else { return }
+    DispatchQueue.global(qos: .background).async { [weak self] in
+      self?.session?.startRunning()
+    }
+  }
+  
+  func checkAccessForCamera() {
+    let status = AVCaptureDevice.authorizationStatus(for: .video)
+    
+    switch status {
+    case .notDetermined:
+      AVCaptureDevice.requestAccess(for: .video) { [weak self] isEnabled in
+        DispatchQueue.main.async {
+          if !isEnabled {
+            self?.showCameraAlert = true
+          }
+        }
+      }
+    case .restricted, .denied:
+      showCameraAlert = true
+      debugPrint("You have explicitly denied permission for media capture")
+    default: break
+    }
+  }
+  
+  func stopRunning() {
+    guard session?.isRunning ?? false else { return }
+    session?.stopRunning()
+  }
+  
+  func updateOutputRectOfInterest(_ size: CGSize) {
+    guard let previewLayer else { return }
+    output.rectOfInterest = previewLayer.metadataOutputRectConverted(
+      fromLayerRect: .init(origin: .zero, size: size))
+  }
+}
+
+extension ScannerViewModel: AVCaptureMetadataOutputObjectsDelegate {
+  
+  func metadataOutput(_ output: AVCaptureMetadataOutput,
+                      didOutput metadataObjects: [AVMetadataObject],
+                      from connection: AVCaptureConnection) {
+    guard let metaData = metadataObjects.first,
+          let readableObject = metaData as? AVMetadataMachineReadableCodeObject else {
+      return
+    }
+    AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+    scannedCode = readableObject.stringValue
+    stopRunning()
+  }
+}
